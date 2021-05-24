ff821ecac76349ec2519e37b65dab99b3879dd94
spacemeowx2 <spacemeowx2@gmail.com>
2021-05-25 01:28:19 +0800

    feat: run server first

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,55 @@
     windows_subsystem = "windows"
 )]
 
+use std::net::SocketAddr;
+
+use anyhow::{Context, Result};
+use rabbit_digger::controller::Controller;
+use rabbit_digger_pro::{api_server, plugin_loader, rabbit_digger};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 mod command;
 mod sys;
 
-fn main() {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerListen {
+    pub access_token: String,
+    pub addr: SocketAddr,
+}
+
+async fn run_server(controller: &Controller) -> Result<ServerListen> {
+    let access_token = Uuid::new_v4().to_string();
+    let addr = api_server::Server {
+        controller: controller.clone(),
+        access_token: Some(access_token.clone()),
+        web_ui: None,
+    }
+    .run("127.0.0.1:0")
+    .await
+    .context("Failed to run api server.")?;
+    Ok(ServerListen { access_token, addr })
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("rabbit_ui=trace"))
+        .init();
+
+    let controller = Controller::new();
+    controller.set_plugin_loader(plugin_loader).await;
+    let server = run_server(&controller).await?;
+
+    log::debug!("server: {:?}", server);
+
     tauri::Builder::default()
+        .manage(controller)
+        .manage(server)
         .invoke_handler(tauri::generate_handler![
             command::get_proxy,
             command::set_proxy,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    Ok(())
 }
